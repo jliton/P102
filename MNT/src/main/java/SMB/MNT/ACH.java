9c2c22200bfe97094e4631d5e6a478a40f93d194
litonjabed <jabedliton1@gmail.com>
2019-01-26 12:00:21 -0500

    t

---
+++ MNT/src/main/java/SMB/MNT/ACH.java
@@ -0,0 +1,9 @@
+package SMB.MNT;
+
+
+
+public class ACH {
+
+	
+	
+}
---
+++ MNT/src/test/java/SMB/MNT/BizPayTest.java
@@ -0,0 +1,13 @@
+package SMB.MNT;
+
+import org.testng.annotations.Test;
+
+public class BizPayTest {
+
+	@Test
+	public void Bizpay() {
+		
+		System.out.println("Bizpay");
+	}
+	
+}
